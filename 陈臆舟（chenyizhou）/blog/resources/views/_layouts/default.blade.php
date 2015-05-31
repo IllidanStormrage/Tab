feba91da2f769fb557a41cefc744f1cc3048b0f3
IllidanStormrae <903446949@qq.com>
2015-05-31 22:03:31 +0800

    Revert "作业"
    This reverts commit 7fec7b69b404b4d97817a8aecf5cde18de23622b.

--- 陈臆舟（chenyizhou）/blog/resources/views/_layouts/default.blade.php
+++
@@ -1,9 +0,0 @@
-
-    @extends('app')
-    @section('content')
-<div class="container-fluid blog-container"><div class="row">
-        <div class="col-md-9 blog-container-list leftsaa">
-            @yield('content')
-
-    @endsection
-
